6b72f1a0b3e588a5a1bc1a59f7aa8a6da42b5b18
moon4sake <choijung4@gmail.com>
2023-08-02 15:26:55 +0900

    Revert "depthwise separable convolution"
    This reverts commit b62a95fbcbedd57501f2303dd0d72b0fa567e7dd.

--- scripts/requirements.sh
+++ scripts/requirements.sh
@@ -2,5 +2,4 @@ pip install jax
 pip install jaxlib
 pip install flax
 pip install tabulate
-# pip install git+https://github.com/cs-giung/giung2-dev.git
-pip install git+https://github.com/cs-giung/giung2-dev.git@acbf4f7f4ce62ea882d3d88a345b1b2a40396b02
+pip install git+https://github.com/cs-giung/giung2-dev.git
